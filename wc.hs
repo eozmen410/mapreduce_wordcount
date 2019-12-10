f5c470421d853b2da554a4aecf807dd9722e065f
Yefri Gaitan <yefrigaitan@gmail.com>
2019-12-10 18:24:06 -0500

    set file structure and add authors info

---
+++ wc.hs
@@ -0,0 +1,26 @@
+module Final where
+import System.IO(Handle, hGetLine, hIsEOF, withFile, IOMode(ReadMode)) -- hIsEOF, hGetLine, withFile, IOMode(ReadMode)
+
+
+{-
+
+ Name: Ecenaz Ozmen and Yefri Gaitan
+ Uni: eo2419 and yg2548
+
+ ------------------------------
+
+ COMS 4995 003 Parallel Functional Programming
+
+ Final Project
+
+ [Description]
+
+ -----
+
+ Use lts-14.5 as the "resolver" for the Haskell Tool Stack.
+
+ Your code should load under GHCi 8.6.5 with no warnings under -Wall, e.g.
+ :set -Wall
+ :l hw4
+
+-}
