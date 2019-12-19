c74112e5991d545c4d5f211fb93e29bcff0e277c
eozmen410 <eo2419@columbia.edu>
2019-12-18 22:11:09 -0500

    readme and file names

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# mapreduce_wordcount
+# Mapreduce Wordcount
+
+Ecenaz Ozmen (eo2419)
+Yefri Gaitan (yg2548)
+
+Parallel Functional Programming 4995 - Final Project
+
+There are 2 different implementations of parallel MapReduce for word counting in this folder. 
+wc_eval.hs contains our initial attempt at parallelizing the task using the Eval Monad.
+wc_eval also has the option to run sequentially, by using the seq option when running as listed below.
+
+to compile:
+ stack ghc -- -O2 -Wall -threaded -rtsopts -eventlog wc
+
+to run sequantial word count:
+ ./wc big.txt seq +RTS -N8 -ls (for sequential)
+to run parallel word count:
+ ./wc big.txt par +RTS -N8 -ls (for parallel)
+
+wc_par.hs contains our improved solution to this problem by using the Par Monad.
+
+to compile:
+ stack ghc -- -O2 -Wall -threaded -rtsopts -eventlog wc
+
+ to run:
+ ./wc big.txt +RTS -N4 -ls 
+
---
+++ wc_par.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE TupleSections #-}
+
+import Control.Parallel(pseq)
+import Control.Parallel.Strategies
+import Data.Char(isAlpha, toLower)
+import Data.Map(Map, keys, fromListWith, toList, unionWith, insert, empty)
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.List(sortBy)
+import Data.Function(on)
+import System.Environment(getArgs, getProgName)
+import System.Exit(die)
+import Stream
+import Control.Monad.Par
+
+{-
+
+ Name: Ecenaz Ozmen and Yefri Gaitan
+ Uni: eo2419 and yg2548
+
+ ------------------------------
+
+ COMS 4995 003 Parallel Functional Programming
+
+ Final Project: MapReduce Word Counter
+
+ to compile:
+ stack ghc -- -O2 -Wall -threaded -rtsopts -eventlog wc
+
+ to run:
+ ./wc big.txt +RTS -N4 -ls
+
+ -----
+
+-}
+
+main :: IO()
+main = do 
+    args <- getArgs
+    case args of 
+        [filename] -> do
+            content <- B.readFile filename
+            print $ length $ pipeline 10000 content 
+        _ -> do 
+            pn <- getProgName
+            die $ "Usage: " ++ pn ++ " <filename>"
+        
+
+wcmap :: Stream B.ByteString -> Par (Stream (B.ByteString, Int))
+wcmap = streamMap (\bs -> (bs, 1))
+
+wcreduce :: Stream (B.ByteString, Int) -> Par (Map B.ByteString Int)
+wcreduce = streamFold (insertTuple) empty
+
+finalreduce :: Stream (Map B.ByteString Int) -> Par (Map B.ByteString Int)
+finalreduce = streamFold (unionWith (+)) empty
+
+pipeline :: Int -> B.ByteString -> [(B.ByteString, Int)]
+pipeline n bs = runPar $ do
+    s0 <- streamFromList (chunk n (map removeNonLetters (B.words bs))) -- stream of lists
+    s1 <- streamMap (runPar . streamFromList) s0 -- make stream of streams
+    s2 <- streamMap (runPar . wcmap) s1 -- gives stream of streams for reduce
+    s3 <- streamMap (runPar . wcreduce) s2 -- stream of maps
+    s4 <- finalreduce s3
+    return $ toList s4
+
+
+-- helper functions
+chunk :: Int -> [a] -> [[a]]
+chunk _ [] = []
+chunk n xs = let (as,bs) = splitAt n xs in as : chunk n bs
+
+removeNonLetters :: B.ByteString -> B.ByteString
+removeNonLetters = B.filter isAlpha . B.map toLower
+
+insertTuple :: Map B.ByteString Int -> (B.ByteString, Int) -> Map B.ByteString Int
+insertTuple m (k,v) = insert k v m
+
